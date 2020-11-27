fdf31b0f6be95943f90d7ae32163aabdb41116b6
avpimblesr <allan.pimble@gmail.com>
2020-11-27 18:41:26 -0500

    Completed UserModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -39,7 +39,7 @@ userSchema.pre('save', function (next) {
   if (user.isModified('password')) {
     // Checking if password field is availabvle and modified
     bcrypt.genSalt(SALT, function (err, salt) {
-      if (err) return next (err)
+      if (err) return next(err)
       bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err)
         user.password = hash;
@@ -47,8 +47,7 @@ userSchema.pre('save', function (next) {
       });
     });
   }
-  else
-  {
+  else {
     next();
   }
 });
@@ -56,9 +55,33 @@ userSchema.pre('save', function (next) {
 // For comparing the user's entered password with database during login
 userSchema.methods.comparePassword = function (candidatePassword, callBack) {
   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return callBack (err);
+    if (err) return callBack(err);
     callBack(null, isMatch);
   });
 }
 
-// for generating token when logged in
+// For generating token when logged in
+userSchema.methods.generateToken = function (callBack) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callBack(err)
+    callBack(null, user)
+  });
+};
+
+// Validating tokenfor auth routes middleware
+userSchema.statics.findByToken = function (token, callBack) {
+  var user = this;
+  jwt.verify(token, process.env.SECRET, function (err, decode) {
+    // This decode must give user_id if token is valid ie decode=user_id
+    user.findOne({ "_id": decode, "token": token }, function (err, user) {
+      if (err) return callBack(err);
+      callBack(null, user);
+    });
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User }
